8301d20551189b7a70237242cfca714f860526ba
yohyow <417682072@qq.com>
2013-11-04 13:35:21 +0800

    init

---
+++ jni/Android.mk
@@ -0,0 +1,28 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := game_shared
+
+LOCAL_MODULE_FILENAME := libgame
+
+# 定义 all-cpp-files 返回当前路径和 Classes 路径想的所有 cpp 文件，注意：这里只考虑 cpp 而没有 c，如果需要自行添加
+#define all-cpp-files
+#$(patsubst jni/%,%, $(shell find $(LOCAL_PATH)/../../Classes/ $(LOCAL_PATH)/hellocpp -name "*.cpp"))  
+#endef
+# 这里使用新的方式替换换来的方式，以自动添加源文件
+LOCAL_SRC_FILES := $(call all-cpp-files)
+
+LOCAL_SRC_FILES := main.cpp \
+                   AppDelegate.cpp \
+                  HelloWorldScene.cpp
+                   
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes                   
+
+LOCAL_WHOLE_STATIC_LIBRARIES := cocos2dx_static cocosdenshion_static cocos_extension_static
+            
+include $(BUILD_SHARED_LIBRARY)
+
+$(call import-module,CocosDenshion/android) \
+$(call import-module,cocos2dx) \
+$(call import-module,extensions)
---
+++ obj/local/armeabi/objs-debug/cocos2dx_static/support/component/CCComponent.o.d
@@ -0,0 +1,32 @@
+obj/local/armeabi/objs-debug/cocos2dx_static/support/component/CCComponent.o: \
+ D:/android-ndk-r9/sources/cocos2dx/support/component/CCComponent.cpp \
+ D:/android-ndk-r9/sources/cocos2dx/support/component/CCComponent.h \
+ D:/android-ndk-r9/sources/cocos2dx/cocoa/CCObject.h \
+ D:/android-ndk-r9/sources/cocos2dx/cocoa/CCDataVisitor.h \
+ D:/android-ndk-r9/sources/cocos2dx/platform/CCPlatformMacros.h \
+ D:/android-ndk-r9/sources/cocos2dx/include/ccConfig.h \
+ D:/android-ndk-r9/sources/cocos2dx/platform/CCPlatformConfig.h \
+ D:/android-ndk-r9/sources/cocos2dx/platform/CCPlatformConfig.h \
+ D:/android-ndk-r9/sources/cocos2dx/platform/android/CCPlatformDefine.h \
+ D:/android-ndk-r9/platforms/android-9/arch-arm/usr/include/android/log.h \
+ D:/android-ndk-r9/sources/cxx-stl/gnu-libstdc++/4.6/include/string
+
+D:/android-ndk-r9/sources/cocos2dx/support/component/CCComponent.h:
+
+D:/android-ndk-r9/sources/cocos2dx/cocoa/CCObject.h:
+
+D:/android-ndk-r9/sources/cocos2dx/cocoa/CCDataVisitor.h:
+
+D:/android-ndk-r9/sources/cocos2dx/platform/CCPlatformMacros.h:
+
+D:/android-ndk-r9/sources/cocos2dx/include/ccConfig.h:
+
+D:/android-ndk-r9/sources/cocos2dx/platform/CCPlatformConfig.h:
+
+D:/android-ndk-r9/sources/cocos2dx/platform/CCPlatformConfig.h:
+
+D:/android-ndk-r9/sources/cocos2dx/platform/android/CCPlatformDefine.h:
+
+D:/android-ndk-r9/platforms/android-9/arch-arm/usr/include/android/log.h:
+
+D:/android-ndk-r9/sources/cxx-stl/gnu-libstdc++/4.6/include/string:
